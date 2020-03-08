dbee6437b8ce553a68419be8df7a293fff23e176
Andrew Leonberger (Local Machine) <andrewnleon@gmail.com>
2020-03-07 23:10:22 -0500

    inmit

--- smh.Api/Program.cs
+++ smh.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace smh.Api
+namespace smh.API
 {
     public class Program
     {
